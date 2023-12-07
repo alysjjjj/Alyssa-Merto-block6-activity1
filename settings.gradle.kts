bbc259a22f3ca9e98d0404b2f096298d7a342b63
alyssa <altu.merto.up@phinmaed.com>
2023-12-08 00:18:00 +0800

    simple calculator app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AlyssaJaneMerto-Block6-Quiz"
+include(":app")
+ 
