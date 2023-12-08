b8330363ff847e7237f363cbf3162dab6a7033a0
alyssa <altu.merto.up@phinmaed.com>
2023-12-08 12:08:49 +0800

    ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.alyssajane.block6.p1.quiz"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.alyssajane.block6.p1.quiz"
